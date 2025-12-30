181713097c09f63d027de28a8e66508bde5bef2a
pr3v3r <pravarsingh@pravars-MacBook-Air.local>
2025-12-30 23:49:26 +0530

    Day 9 : Login controller and testing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -33,4 +35,50 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: error.message });
       }
       
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Check inputs
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    // 2. Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // 3. Compare password
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // 4. Generate token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // 5. Send response (NO password)
+    res.status(200).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
